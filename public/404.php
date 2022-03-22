f619752c42e6baec6d7f6eb9219f245821f0fff0
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2022-03-22 14:17:19 +0100

    On met une page 404 dans le .htaccess

---
+++ public/404.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404</title>
+	<link rel="shortcut icon" type="image/x-icon" href="LOGO-ENCART.png" />
+	<link rel="stylesheet" href="404.css">
+</head>
+<body>
+<div id="container">
+    <div class="content">
+        <h2>404</h2>
+        <h4>Oups ! La page est introuvable</h4>
+        <p>La page que vous cherchez n'existe pas. Vous avez peut-être mal écrit l'adresse ou la page a peut-être été
+        supprimé.</p>
+        <a href="index.php">Retour vers l'acceuil</a>
+    </div>
+</div>
+<script src="404.js"></script>
+</body>
+</html>
