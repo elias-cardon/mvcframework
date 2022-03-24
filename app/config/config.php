ffdf85339ea5f3f35cfeff20ef1ff04a0715c7b2
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2022-03-24 15:56:24 +0100

    On s'occupe de l'URLROOT

--- app/config/config.php
+++ app/config/config.php
@@ -6,4 +6,8 @@ define('DB_USER', '');
 define('DB_PASS', '');
 define('DB_NAME', '');
 
-echo __FILE__;
+//APPROOT
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URLROOT
+define('URLROOT', 'localhost/mvcframework'); //Lien à changer à chaque nouveau projet
