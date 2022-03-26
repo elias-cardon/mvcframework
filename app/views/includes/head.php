ecfff92b4fca8a7d08f4e54de1fd829ea82672b3
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2022-03-26 18:14:08 +0100

    On corrige pour appliquer le CSS

--- app/views/includes/head.php
+++ app/views/includes/head.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo SITENAME; ?></title>
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo URLROOT ?>/public/assets/css/style.css">
     <!-- Google fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
