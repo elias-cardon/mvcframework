1fe927ea3f8d2fac5fee64a85fbc153ebe9e5ca2
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2022-03-26 18:14:56 +0100

    On corrige pour appliquer le CSS

--- app/config/config.php
+++ app/config/config.php
@@ -11,6 +11,7 @@ define('APPROOT', dirname(dirname(__FILE__)));
 
 //URLROOT
 define('URLROOT', 'http://localhost/mvcframework'); //Lien à changer à chaque nouveau projet -- URLROOT permet de ne pas avoir à copier-coller le l'url à chaque fois
+//Le CSS ne marchait pas si je mettais pas le http:// dans l'url
 
 //Nom du site
 define('SITENAME', 'MVC Framework');
